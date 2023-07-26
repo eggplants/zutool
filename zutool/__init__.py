756b99ad70ef88e845bb1a4cbc7712e6f470d33f
eggplants <w10776e8w@yahoo.co.jp>
2023-07-27 03:25:53 +0900

    v0.0.4

--- zutool/__init__.py
+++ zutool/__init__.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from .api import get_pain_status, get_weather_point, get_weather_status
 
-__version__ = "0.0.3"
+__version__ = "0.0.4"
 __all__ = (
     "get_pain_status",
     "get_weather_point",
