63e4374f7fca4ced0e45098dab2ccae8962acacf
eggplants <w10776e8w@yahoo.co.jp>
2024-10-03 23:29:34 +0900

    v0.1.1

--- zutool/__init__.py
+++ zutool/__init__.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from .api import get_otenki_asp, get_pain_status, get_weather_point, get_weather_status
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __all__ = (
     "get_otenki_asp",
     "get_pain_status",
