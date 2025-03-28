0bbd91792b2e43b5dd5c50b4cf74637a419fe49e
haruna <w10776e8w@yahoo.co.jp>
2025-03-28 09:56:16 +0900

    fix generation of uv completion

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -2,11 +2,12 @@
 
 set -euxo pipefail
 
-uv generate-shell-completion zsh
 uv sync --dev --all-extras
 uv run pre-commit install --allow-missing-config
 
 cat<<'A'>> ~/.zshrc
+eval "$(uv generate-shell-completion zsh)"
+eval "$(uvx --generate-shell-completion zsh)"
 command -v deactivate &>/dev/null || . .venv/bin/activate
 A
 
